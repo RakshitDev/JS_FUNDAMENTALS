43037dfb0ff03995bc3ebf79865e71cbba0a50ba
Rakshit <rbalagundi@gmail.com>
2026-09-04 22:19:38 +0530

    intersection observer api added

--- AdvanceDom/Bankist/script.js
+++ AdvanceDom/Bankist/script.js
@@ -117,3 +117,27 @@ btnContainer.addEventListener("click", function (e) {
 
   content.classList.add("operations__content--active");
 });
+
+// Adding the sticky  nav on reaching  the section 1
+// first select those elements
+// Adding the sticky nav on reaching the section 1
+const header = document.querySelector(".header");
+const nav = document.querySelector(".nav");
+const navHeight = nav.getBoundingClientRect().height;
+
+const stickyNav = function (entries) {
+  const [entry] = entries;
+  if (!entry.isIntersecting) {
+    nav.classList.add("sticky");
+  } else {
+    nav.classList.remove("sticky");
+  }
+};
+
+const headerObserver = new IntersectionObserver(stickyNav, {
+  root: null,
+  threshold: 0,
+  // rootMargin: `-${navHeight}px`,
+});
+
+headerObserver.observe(header);
